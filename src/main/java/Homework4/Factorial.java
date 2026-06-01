f4013461df8b9a288aab50f37fe69f761ff718fb
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-01 23:33:47 +0400

    [Tatia Idukashvili] HW4 - Factorial

--- src/main/java/Homework4/Factorial.java
+++ src/main/java/Homework4/Factorial.java
@@ -1,4 +1,16 @@
+/** @author Tatia Idukashvili */
+
 package Homework4;
 
 public class Factorial {
+    public static void main(String[] args) {
+        int birthMonth = 7;
+        long factorial = 1;
+
+        for (int i = 1; i <= birthMonth; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(birthMonth + "! = " + factorial);
+    }
 }
