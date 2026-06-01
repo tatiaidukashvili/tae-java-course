68577525ff8b331f2a250891873772699df47a4d
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-01 23:41:41 +0400

    [Tatia Idukashvili] HW4 - SumOfDigits

--- src/main/java/Homework4/SumOfDigits.java
+++ src/main/java/Homework4/SumOfDigits.java
@@ -1,4 +1,17 @@
+/** @author Tatia Idukashvili */
+
 package Homework4;
 
 public class SumOfDigits {
+    public static void main(String[] args) {
+        int birthYear = 2007;
+        int sum = 0;
+
+        while (birthYear > 0) {
+            sum += birthYear % 10;
+            birthYear /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
 }
