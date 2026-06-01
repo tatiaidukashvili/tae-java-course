81807fd9b0cb082a189d62d8d1f2f10b9765d7b7
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-01 23:08:28 +0400

    [Tatia Idukashvili] HW4 - Calculator

--- src/main/java/Homework4/Calculator.java
+++ src/main/java/Homework4/Calculator.java
@@ -1,4 +1,36 @@
+/** @author Tatia Idukashvili */
 package Homework4;
 
 public class Calculator {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 0;
+        char operator = '/';
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '-':
+                System.out.println(a - b);
+                break;
+
+            case '*':
+                System.out.println(a * b);
+                break;
+
+            case '/':
+                if (b == 0) {
+                    System.out.println("Division by zero is not allowed");
+                }
+                else {
+                    System.out.println(a / b);
+                }
+                break;
+
+            default:
+                System.out.println("Unknown operator");
+        }
+    }
 }
