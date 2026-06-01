0aa50db94d4acf10cd4e10f3a27ac1ac4bc6d418
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-01 22:49:05 +0400

    [Tatia Idukashvili] HW4 - FizzBuzz

---
+++ src/main/java/Homework4/FizzBuzz.java
@@ -0,0 +1,22 @@
+/** @author Tatia Idukashvili */
+
+package Homework4;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 54; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
