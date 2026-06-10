3a3042d19f96ae846aecb5caef84d19e3dfce801
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-10 14:05:37 +0400

    [Tatia Idukashvili] HW6 - Counter

--- src/main/java/homework6/Counter.java
+++ src/main/java/homework6/Counter.java
@@ -1,4 +1,37 @@
+/** @author <Tatia Idukashvili> */
+
 package homework6;
 
 public class Counter {
+    int count;
+    int maxValue;
+
+    public Counter() {
+        count = 0;
+        maxValue = 100;
+    }
+
+    void increment() {
+        if (count < maxValue) {
+            count++;
+        }
+    }
+
+    void reset() {
+        count = 0;
+    }
+
+    void countTo(int target) {
+        for (int i = 1; i <=  target; i++) {
+            increment();
+        }
+    }
+
+    public static void main(String[] args) {
+        Counter c = new Counter();
+
+        c.countTo(5);
+
+        System.out.println(c.count);
+    }
 }
