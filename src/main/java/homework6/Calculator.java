9d53afd3a7b821cfabe052aa3f90a7c76f399bc1
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-10 17:51:21 +0400

    [Tatia Idukashvili] HW6 - Calculator

--- src/main/java/homework6/Calculator.java
+++ src/main/java/homework6/Calculator.java
@@ -1,4 +1,37 @@
+/** @author <Tatia Idukashvili> */
 package homework6;
 
 public class Calculator {
+    public double calculate(double a, double b, char op) {
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b != 0) {
+                    return a / b;
+                }
+                return Double.NaN;
+
+            default:
+                return Double.NaN;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.calculate(10, 3, '+'));
+
+        System.out.println(calc.calculate(8, 7, '-'));
+
+        System.out.println(calc.calculate(9, 8, '*'));
+
+        System.out.println(calc.calculate(15, 3, '/'));
+
+        System.out.println(calc.calculate(10, 0, '/'));
+    }
 }
