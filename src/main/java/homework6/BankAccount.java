a1d036dbc4f52341e3fddce77bc6a41d0a76c879
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-10 13:59:34 +0400

    [Tatia Idukashvili] HW6 - BankAccount

--- src/main/java/homework6/BankAccount.java
+++ src/main/java/homework6/BankAccount.java
@@ -1,4 +1,40 @@
+/** @author <Tatia Idukashvili> */
+
 package homework6;
 
 public class BankAccount {
+    private String owner;
+    private double balance;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("არასაკმარისი თანხა");
+        }
+        else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount("Tatia", 100);
+
+        acc.deposit(50);
+
+        System.out.println(acc.getBalance());
+    }
 }
