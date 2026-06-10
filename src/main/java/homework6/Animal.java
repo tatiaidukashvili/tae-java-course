18b792369b4b138c54f4e1250bf313c21214d201
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-10 14:13:13 +0400

    [Tatia Idukashvili] HW6 - AnimalDog

--- src/main/java/homework6/Animal.java
+++ src/main/java/homework6/Animal.java
@@ -1,4 +1,15 @@
+/** @author <Tatia Idukashvili> */
+
 package homework6;
 
 public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("some sound");
+    }
 }
--- src/main/java/homework6/Dog.java
+++ src/main/java/homework6/Dog.java
@@ -1,4 +1,17 @@
+/** @author <Tatia Idukashvili> */
 package homework6;
 
-public class Dog {
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + ": bark!");
+    }
+
+    public static void main(String[] args) {
+        new Dog("Rex").makeSound();
+    }
 }
