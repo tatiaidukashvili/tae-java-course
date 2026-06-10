0ea058ba9837fbe730fe23b01f4d5e27fe28b184
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-10 17:43:33 +0400

    [Tatia Idukashvili] HW6 - VehicleCar

--- src/main/java/homework6/Vehicle.java
+++ src/main/java/homework6/Vehicle.java
@@ -1,4 +1,16 @@
+/** @author <Tatia Idukashvili> */
 package homework6;
 
 public class Vehicle {
+    protected String brand;
+    protected int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public void info() {
+        System.out.println(brand + " " + year);
+    }
 }
