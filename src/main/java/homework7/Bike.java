334229a9eba7ccb3a0a4f1b907836199c9a7809f
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-11 12:14:50 +0400

    [Tatia Idukashvili] HW7 - VehicleCarBike

--- src/main/java/homework7/Bike.java
+++ src/main/java/homework7/Bike.java
@@ -1,4 +1,15 @@
+/** @author <Tatia Idukashvili> */
+
 package homework7;
 
-public class Bike {
+public class Bike extends Vehicle {
+
+    public Bike(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(brand + " pedaling");
+    }
 }
--- src/main/java/homework7/Vehicle.java
+++ src/main/java/homework7/Vehicle.java
@@ -1,4 +1,18 @@
+/** @author <Tatia Idukashvili> */
+
 package homework7;
 
-public class Vehicle {
+public abstract class Vehicle {
+
+    protected String brand;
+
+    public Vehicle(String brand) {
+        this.brand = brand;
+    }
+
+    public abstract void start();
+
+    public void stop() {
+        System.out.println(brand + " stopped");
+    }
 }
