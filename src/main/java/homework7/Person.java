38bc6df3723a479ffb9501897fb536a5fb6fd2d7
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-11 12:22:00 +0400

    [Tatia Idukashvili] HW7 - Person

--- src/main/java/homework7/Person.java
+++ src/main/java/homework7/Person.java
@@ -1,4 +1,39 @@
-package homework7;
+/** @author <Tatia Idukashvili> */
 
-public class Person {
+package homework7;
+import java.util.Arrays;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+
+    public static void main(String[] args) {
+        Person[] people = {
+                new Person("Anna", 25),
+                new Person("Beka", 30),
+                new Person("Cita", 22),
+                new Person("Tatia", 18),
+        };
+
+        Arrays.sort(people);
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
 }
