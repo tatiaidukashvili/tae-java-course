b2bcaf1dd515d9b33ea45f975bc03ed78c918b50
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-11 11:44:48 +0400

    [Tatia Idukashvili] HW7 - ShapeRectangleCircle

---
+++ src/main/java/homework7/Circle.java
@@ -0,0 +1,16 @@
+/** @author <Tatia Idukashvili> */
+
+package homework7;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/homework7/Shape.java
@@ -0,0 +1,7 @@
+/** @author <Tatia Idukashvili> */
+
+package homework7;
+
+public interface Shape {
+    double area();
+}
