e1cb0ce703b25c23a499784b6c5d160299343a9f
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-11 11:17:44 +0400

    [Tatia Idukashvili] HW6 - Student

--- src/main/java/homework6/Student.java
+++ src/main/java/homework6/Student.java
@@ -1,4 +1,37 @@
+/** @author <Tatia Idukashvili> */
 package homework6;
 
 public class Student {
+    String name;
+    int[] grades;
+
+    public Student(String name, int[] grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public double averageGrade() {
+        int sum = 0;
+
+        for (int grade : grades) {
+            sum += grade;
+        }
+
+        return (double) sum / grades.length;
+    }
+
+    public String getStatus() {
+        if (averageGrade() >= 60) {
+            return "Pass";
+        }
+        else {
+            return "Fail";
+        }
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("ანა", new int[] {85, 92, 78});
+
+        System.out.println(student.getStatus());
+    }
 }
