f166ab3e7f6324959f61200d7ab045a765283596
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-16 18:30:36 +0400

    [Tatia Idukashvili] HW9 - WordCounter

--- src/main/java/homework9/WordCounter.java
+++ src/main/java/homework9/WordCounter.java
@@ -1,4 +1,26 @@
+/** @author <Tatia Idukashvili> */
+
 package homework9;
 
+import java.util.HashMap;
+
 public class WordCounter {
+    public static void main(String[] args) {
+        String text = "java git java oop java git python";
+
+        String[] words = text.split(" ");
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            }
+            else {
+                map.put(word, 1);
+            }
+        }
+
+        System.out.println(map);
+    }
 }
