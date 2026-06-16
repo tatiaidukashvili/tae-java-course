da0673b9c6737400923b01f326bdbc4a841e362c
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-16 12:47:37 +0400

    [Tatia Idukashvili] HW8 - InvalidAgeExceptionAndAgeValidator

--- src/main/java/homework8/InvalidAgeException.java
+++ src/main/java/homework8/InvalidAgeException.java
@@ -1,4 +1,9 @@
+/** @author <Tatia Idukashvili>*/
+
 package homework8;
 
-public class InvalidAgeException {
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
 }
