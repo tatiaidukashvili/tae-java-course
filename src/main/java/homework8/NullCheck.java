7961000ddcb0919f4edad877ffcc6cd3b7ed9eab
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-16 12:24:13 +0400

    [Tatia Idukashvili] HW8 - NullCheck

--- src/main/java/homework8/NullCheck.java
+++ src/main/java/homework8/NullCheck.java
@@ -1,4 +1,20 @@
+/** @author <Tatia Idukashvili>*/
+
 package homework8;
 
 public class NullCheck {
+    public static int safeLength(String s) {
+        try {
+            return s.length();
+        }
+        catch (NullPointerException e) {
+            System.out.println("null სტრიქონი");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(safeLength("Hello"));
+        System.out.println(safeLength(null));
+    }
 }
