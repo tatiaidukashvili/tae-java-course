8c7f43458ee94b997aee52f3986c4334c113de4f
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-16 12:31:50 +0400

    [Tatia Idukashvili] HW8 - MultiCatchDemo

--- src/main/java/homework8/MultiCatchDemo.java
+++ src/main/java/homework8/MultiCatchDemo.java
@@ -1,4 +1,30 @@
+/** @author <Tatia Idukashvili>*/
+
 package homework8;
 
 public class MultiCatchDemo {
+    public static void processInput(String input) {
+        try {
+            int result = Integer.parseInt(input);
+
+            int[] arr = {10, 20};
+
+            System.out.println(arr[result] / result);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("NumberFormatException");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("ArithmeticException");
+        }
+    }
+
+    public static void main(String[] args) {
+        processInput("abc");
+        processInput("0");
+        processInput("99");
+    }
 }
