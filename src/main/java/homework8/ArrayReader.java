b2783726ffb3abe66248ddc48fe442fd354d7da3
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-16 11:54:23 +0400

    [Tatia Idukashvili] HW8 - ArrayReader

--- src/main/java/homework8/ArrayReader.java
+++ src/main/java/homework8/ArrayReader.java
@@ -1,4 +1,22 @@
+/** @author <Tatia Idukashvili>*/
+
 package homework8;
 
 public class ArrayReader {
+    public static int readSafe(int[] arr, int index) {
+        try {
+            return arr[index];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ინდექსი არასწორი: " + index);
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+
+        System.out.println(readSafe(nums, 1));
+        System.out.println(readSafe(nums, 10));
+    }
 }
