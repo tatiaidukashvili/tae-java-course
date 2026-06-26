056943df446240ede0b1e509ffca6f8f43b16bf0
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-26 23:18:12 +0400

    [Tatia Idukashvili] HW10 - FilterStream

--- src/main/java/homework10/FilterStream.java
+++ src/main/java/homework10/FilterStream.java
@@ -1,4 +1,15 @@
+/** @author <Tatia Idukashvili> */
 package homework10;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FilterStream {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> evens = nums.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+
+        System.out.println(evens);
+    }
 }
