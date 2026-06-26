ff4ee48524b5726698ca9ad373c72946dd221d75
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-26 23:55:12 +0400

    [Tatia Idukashvili] HW10 - WordCounterStream

--- src/main/java/homework10/WordCounterStream.java
+++ src/main/java/homework10/WordCounterStream.java
@@ -1,4 +1,18 @@
+/** @author <Tatia Idukashvili> */
 package homework10;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class WordCounterStream {
+    public static void main(String[] args) {
+        String text = "java git java oop java git python";
+
+        String[] words = text.split(" ");
+
+        Map<String, Long> counts = Arrays.stream(words).collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        System.out.println(counts);
+    }
 }
