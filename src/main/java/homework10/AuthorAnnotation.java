4806382e1aab864bf200126d6f71a73ba41f12ee
Tatia Idukashvili <tidukashvili@credo.ge>
2026-06-26 23:50:02 +0400

    [Tatia Idukashvili] HW10 - AuthorAnnotationDemo

--- src/main/java/homework10/AuthorAnnotation.java
+++ src/main/java/homework10/AuthorAnnotation.java
@@ -1,4 +1,17 @@
+/** @author <Tatia Idukashvili> */
 package homework10;
 
-public class AuthorAnnotation {
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+@interface Author {
+
+    String name();
+
+    String date() default "unknown";
 }
